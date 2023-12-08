f9f59a3f2531a21072ea596c464882081ff2598a
omar <omarhassan11738@gmail.com>
2023-12-08 09:01:57 +0200

    private messages no longer appear in general

---
+++ server.php
@@ -0,0 +1,357 @@
+<?php
+session_start();
+
+// Check if 'idnumber' is set and not empty
+if (!isset($_POST['idnumber']) || empty($_POST['idnumber'])) {
+    header("Location: index.html");
+    exit();
+}
+
+// Assuming your database credentials
+$host = "localhost";
+$username_db = "root";
+$password_db = "";
+$database = "sharkawi_muc";
+
+// Create a connection to the database
+$conn = new mysqli($host, $username_db, $password_db, $database);
+
+// Check the connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+// 'idnumber' from the form
+$username = $_POST['idnumber'];
+
+// Fetch email from the users table where id equals $username
+$sql = "SELECT email FROM users WHERE id = '$username'";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    // Fetch the first row
+    // Fetch the first row
+    $row = $result->fetch_assoc();
+
+    // Get the email
+    $email = $row['email'];
+
+    // Remove the last 20 characters
+    $emailWithoutLast20 = substr($email, 0, -20);
+
+    // Get the remaining characters
+    $name = $emailWithoutLast20;
+} else {
+    echo "";
+}
+
+// Close the database connection when you're done
+$conn->close();
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Live Chat</title>
+    <link rel="stylesheet" href="./chat-style.css" />
+</head>
+
+<body>
+    <div id="container">
+        <div id="sidebar">
+            <div id="welcome-message">Welcome,
+                <?php echo    $name; ?>!
+            </div>
+            <h3>Conversations</h3>
+            <a href="#" class="conversation-link" data-channel="General">General</a>
+            <?php
+            if (substr($username, 0, 4) === "2211") {
+                echo '<a href="#" class="conversation-link" data-channel="PhysicalTherapy">Physical Therapy</a>';
+            } elseif (substr($username, 0, 4) === "2212") {
+                echo '<a href="#" class="conversation-link" data-channel="Engineering">Engineering</a>';
+            } elseif (substr($username, 0, 4) === "2213") {
+                echo '<a href="#" class="conversation-link" data-channel="Business">Business</a>';
+            }
+            ?>
+            <div id="online-users-container">
+                <h3>Online Users</h3>
+                <div id="online-users"></div>
+            </div>
+            <a href="logout.php" class="logout-button">Logout</a>
+        </div>
+        <div id="chat-container">
+            <div id="chat-messages"></div>
+            <div>
+                <input type="text" id="message-input" placeholder="Type your message..." class="input">
+                <select id="category-select">
+                    <option value="General">General</option>
+                    <option value="Engineering">Engineering</option>
+                    <option value="PhysicalTherapy">Physical Therapy</option>
+                    <option value="Business">Business</option>
+                </select>
+                <!-- Add input field for shift value -->
+                <label for="shift-input">Shift Value:</label>
+                <input type="number" id="shift-input" min="1" value="3" class="input">
+                <button id="send-button" class="button">Send</button>
+            </div>
+        </div>
+    </div>
+
+    <script>
+        const ws = new WebSocket('ws://localhost:8080?username=<?php echo $name; ?>');
+        const userName = "<?php echo $name; ?>";
+        let currentChannel = "General";
+
+        function sendMessage() {
+            const messageInput = document.getElementById('message-input');
+            const categorySelect = document.getElementById('category-select');
+            const shiftInput = document.getElementById('shift-input'); // Get the shift input
+            const message = messageInput.value.trim();
+            const category = categorySelect.value || "General";
+            shiftValue = parseInt(shiftInput.value) || 3; // Get the shift value or default to 3
+
+            // Function to encrypt a message using Caesar cipher with the specified shift value
+            function encryptMessage(text, shift) {
+                return [...text]
+                    .map(char => {
+                        const charCode = char.charCodeAt(0);
+                        if (charCode >= 65 && charCode <= 90) {
+                            return String.fromCharCode((charCode - 65 + shift) % 26 + 65); // Uppercase letters
+                        } else if (charCode >= 97 && charCode <= 122) {
+                            return String.fromCharCode((charCode - 97 + shift) % 26 + 97); // Lowercase letters
+                        } else {
+                            return char; // Non-alphabetic characters
+                        }
+                    })
+                    .join('');
+            }
+
+            if (message !== '') {
+                // Encrypt the message using Caesar cipher with the specified shift value
+                const encryptedMessage = encryptMessage(message, shiftValue);
+
+                const fullMessage = `${userName}:${category}:${encryptedMessage}`;
+                ws.send(fullMessage);
+
+                const data = {
+                    sender: encodeURIComponent(userName),
+                    message: encodeURIComponent(encryptedMessage),
+                    category: encodeURIComponent(category)
+                };
+                fetch('insert_message.php', {
+                        method: 'POST',
+                        headers: {
+                            'Content-type': 'application/x-www-form-urlencoded'
+                        },
+                        body: Object.keys(data).map(key => `${key}=${data[key]}`).join('&')
+                    })
+                    .then(response => {
+                        if (!response.ok) {
+                            throw new Error('Network response was not ok');
+                        }
+                        return response.text();
+                    })
+                // const xhr = new XMLHttpRequest();
+                // xhr.open('POST', 'insert_message.php', true);
+                // xhr.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+                // xhr.send(`sender=${encodeURIComponent(userName)}&message=${encodeURIComponent(encryptedMessage)}&category=${encodeURIComponent(category)}`);
+
+                messageInput.value = '';
+            }
+        }
+        // assume model is DB
+        // model
+
+        // view 
+        function clearChatMessages() {
+            const chatMessages = document.getElementById('chat-messages');
+            chatMessages.innerHTML = '';
+        }
+
+        // controller
+
+
+        function updateOnlineUsers(onlineUsers) {
+            const onlineUsersElement = document.getElementById('online-users');
+            onlineUsersElement.innerHTML = '<ul id="online-users-list"></ul>';
+
+            const onlineUsersList = document.getElementById('online-users-list');
+            const categorySelect = document.getElementById('category-select');
+
+            onlineUsers.split(',').forEach(username => {
+                const listItem = document.createElement('li');
+                const link = document.createElement('li');
+
+                link.setAttribute('data-channel', username);
+                link.textContent = username;
+                link.setAttribute('class', 'online-user');
+
+                link.addEventListener('click', function() {
+                    clearChatMessages();
+
+                    let option = categorySelect.querySelector(`option[value="${username}"]`);
+                    if (!option) {
+                        option = document.createElement('option');
+                        option.value = username;
+                        option.textContent = username;
+                        categorySelect.appendChild(option);
+
+                        // Create a new conversation link
+                        const conversationLink = document.createElement('a');
+                        conversationLink.href = '#';
+                        conversationLink.setAttribute('data-channel', username);
+                        conversationLink.textContent = username;
+                        conversationLink.classList.add('conversation-link');
+
+                        // Add click event listener to the new conversation link
+                        conversationLink.addEventListener('click', function(event) {
+                            event.preventDefault();
+                            const channel = this.getAttribute('data-channel');
+                            currentChannel = channel;
+                            loadMessages(currentChannel);
+                        });
+
+                        // Append the new conversation link to the conversations list
+                        document.getElementById('sidebar').insertBefore(conversationLink, document.getElementById('online-users-container'));
+                    }
+
+                    option.selected = true;
+                    event.preventDefault();
+                    const channel = this.getAttribute('data-channel');
+                    currentChannel = channel;
+                    loadMessages(currentChannel);
+                });
+
+                listItem.appendChild(link);
+                onlineUsersList.appendChild(listItem);
+            });
+        }
+
+
+
+        function appendMessage(message, userClass) {
+            const chatMessages = document.getElementById('chat-messages');
+            const messageContainer = document.createElement('div');
+            messageContainer.className = `message-container ${userClass}`;
+
+            const idChannelContainer = document.createElement('div');
+            idChannelContainer.className = 'id-channel-container';
+
+            const idChannelText = document.createElement('span');
+            idChannelText.className = 'id-channel-text';
+            idChannelText.textContent = message.split(': ')[0];
+
+            const messageText = document.createElement('span');
+            messageText.className = 'message-text';
+            const encryptedMessage = message.split(': ').slice(1).join(':');
+            messageText.textContent = encryptedMessage;
+
+            messageContainer.addEventListener('click', toggleMessage);
+
+            function toggleMessage() {
+
+                if (messageText.textContent === encryptedMessage) {
+                    let decrypt_shift = +prompt('Enter shift value');
+                    // If the current content is the encrypted message, decrypt it
+                    messageText.textContent = decryptMessage(encryptedMessage, decrypt_shift);
+                } else {
+                    // If the current content is decrypted, revert to the encrypted message
+                    messageText.textContent = encryptedMessage;
+                }
+            }
+            idChannelContainer.appendChild(idChannelText);
+            idChannelContainer.appendChild(document.createTextNode(':'));
+            messageContainer.appendChild(idChannelContainer);
+            messageContainer.appendChild(messageText);
+            chatMessages.appendChild(messageContainer);
+            chatMessages.scrollTop = chatMessages.scrollHeight;
+        }
+
+        function decryptMessage(text, shift) {
+            return [...text]
+                .map(char => {
+                    const charCode = char.charCodeAt(0);
+                    if (charCode >= 65 && charCode <= 90) {
+                        return String.fromCharCode(((charCode - 65 - shift + 26 * 999999999) % 26) + 65); // Uppercase letters
+                    } else if (charCode >= 97 && charCode <= 122) {
+                        return String.fromCharCode(((charCode - 97 - shift + 26 * 999999999) % 26) + 97); // Lowercase letters
+                    } else {
+                        return char; // Non-alphabetic characters
+                    }
+                })
+                .join('');
+        }
+
+
+        document.getElementById('send-button').addEventListener('click', function() {
+            sendMessage();
+        });
+
+        document.getElementById('message-input').addEventListener('keydown', function(event) {
+            if (event.key === 'Enter') {
+                event.preventDefault();
+                sendMessage();
+            }
+        });
+
+        ws.onmessage = function(event) {
+            const parts = event.data.split(':');
+            const sender = parts[0];
+            const category = parts[1] || "General";
+            const message = parts.slice(2).join(':');
+            const isCurrentUser = sender === userName;
+            const userClass = isCurrentUser ? 'user-message' : 'other-message';
+            // appendMessage(`${sender} (${category}): ${message}`, userClass);
+
+            // Check if the message belongs to the current channel
+            if (category === currentChannel) {
+                appendMessage(`${sender} (${category}): ${message}`, userClass);
+            }
+
+            if (event.data.startsWith('Online Users:')) {
+                const onlineUsers = event.data.replace('Online Users: ', '');
+                updateOnlineUsers(onlineUsers);
+            }
+        };
+
+        window.onload = function() {
+            loadMessages(currentChannel);
+            document.getElementById('welcome-message').style.display = 'block';
+            loadOnlineUsers();
+        };
+
+        function loadMessages(channel) {
+            clearChatMessages();
+
+            fetch(`get_messages.php?category=${encodeURIComponent(channel)}`).then(res => res.json()).then(messages =>
+                messages.reverse().forEach(function(message) {
+                    const isCurrentUser = message.sender === userName;
+                    const userClass = isCurrentUser ? 'user-message' : 'other-message';
+                    appendMessage(`${message.sender} (${message.category}): ${message.message}`, userClass);
+                })
+            )
+
+
+            document.getElementById('category-select').value = channel;
+        }
+
+        document.querySelectorAll('.conversation-link').forEach(function(link) {
+            link.addEventListener('click', function(event) {
+                event.preventDefault();
+                const channel = this.getAttribute('data-channel');
+                currentChannel = channel;
+                loadMessages(currentChannel);
+            });
+        });
+
+        function loadOnlineUsers() {
+            ws.send("Get Online Users");
+        }
+    </script>
+</body>
+
+</html>
